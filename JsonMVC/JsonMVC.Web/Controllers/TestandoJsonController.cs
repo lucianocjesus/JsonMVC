b862c8f7c1606258148d23f94e958f2fc8795b14
Luciano Carlos - Pearson <luciano.jesus@pearson.com>
2016-09-16 14:55:59 -0300

    Evolução de codigo

---
+++ JsonMVC/JsonMVC.Web/Controllers/TestandoJsonController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using System.Web.Mvc;
+
+namespace JsonMVC.Web.Controllers
+{
+    public class TestandoJsonController : Controller
+    {
+        // GET: TestandoJson
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public JsonResult GetDados()
+        {
+            var resultado = new List<object>();
+            
+            resultado.Add(new
+            {
+                nome = "Luciano Carlos de Jesus",
+                telefone = "19982066377"
+            });
+
+            resultado.Add(new
+            {
+                nome = "Simone Maria Malaquias",
+                telefone = "19991924904"
+            });
+
+            resultado.Add(new
+            {
+                nome = "Jordã Carlos de Jesus",
+                telefone = "19999525160"
+            });
+
+            return Json(resultado, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
---
+++ JsonMVC/JsonMVC.Web/Scripts/functions.js
@@ -0,0 +1,17 @@
+﻿$(function () {
+    $.ajax({
+        dataType: "json",
+        type: "GET",
+        url: "/TestandoJson/GetDados",
+        success: function (dados) {
+            $(dados).each(function (i) {
+                var p = "Nome: " + dados[i].nome + " - Telefone: " + dados[i].telefone;
+                $(".inner").append("<p>" + p + "</p>");
+            });
+        },
+        error: function () {
+            $('#lblErro').html('Erro ao trazer os dados.');
+            $('#modalAlerta').modal();
+        }
+    });
+})
