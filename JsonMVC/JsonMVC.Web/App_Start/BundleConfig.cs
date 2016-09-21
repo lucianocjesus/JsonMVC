0bf10be73ec7e38f38666df6587114abc40aa4f8
lucianocjesus - Luciano Carlos <lucianocjesus@gmail.com>
2016-09-20 23:04:20 -0300

    Incorporado thema para master.

--- JsonMVC/JsonMVC.Web/App_Start/BundleConfig.cs
+++ JsonMVC/JsonMVC.Web/App_Start/BundleConfig.cs
@@ -26,15 +26,54 @@ namespace JsonMVC.Web
                       "~/Scripts/bootstrap.js",
                       "~/Scripts/respond.js"));
 
+            //Bundles de scripts para templante bucket
+            bundles.Add(new ScriptBundle("~/bundles/themebucket").Include(
+                        "~/Scripts/themebucket/clndr.js",
+                        "~/Scripts/themebucket/dashboard.js",
+                        "~/Scripts/themebucket/skycons.js",
+                        "~/Scripts/themebucket/evnt.calendar.init.js",
+                        "~/Scripts/themebucket/excanvas.min.js",
+                        "~/Scripts/themebucket/gauge.js",
+                        "~/Scripts/themebucket/jquery-jvectormap-1.2.2.min.js",
+                        "~/Scripts/themebucket/jquery-jvectormap-us-lcc-en.js",
+                        "~/Scripts/themebucket/jquery-ui-1.10.1.custom.min.js",
+                        "~/Scripts/themebucket/jquery.customSelect.min.js",
+                        "~/Scripts/themebucket/jquery.dcjqaccordion.2.7.js",
+                        "~/Scripts/themebucket/jquery.easypiechart.js",
+                        "~/Scripts/themebucket/jquery.flot.animator.min.js",
+                        "~/Scripts/themebucket/jquery.flot.growraf.js",
+                        "~/Scripts/themebucket/jquery.flot.js",
+                        "~/Scripts/themebucket/jquery.flot.pie.resize.js",
+                        "~/Scripts/themebucket/jquery.flot.resize.js",
+                        "~/Scripts/themebucket/jquery.flot.tooltip.min.js",
+                        "~/Scripts/themebucket/jquery.nicescroll.js",
+                        "~/Scripts/themebucket/jquery.scrollTo.js",
+                        "~/Scripts/themebucket/jquery.scrollTo.min.js",
+                        "~/Scripts/themebucket/jquery.slimscroll.js",
+                        "~/Scripts/themebucket/jquery.sparkline.js",
+                        "~/Scripts/themebucket/moment-2.2.1.js",
+                        "~/Scripts/themebucket/scripts.js"));
+
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/bootstrap-theme.css",
+                      "~/font-awesome/css/font-awesome.min.css",
                       "~/Content/site.css"));
 
-            bundles.Add(new ScriptBundle("~/Content/login").Include(
-                        "~/font-awesome/css/font-awesome.min.css",
+            //Bundles de css para pagina de login
+            bundles.Add(new StyleBundle("~/Content/login").Include(
                         "~/Content/animate.css",
-                        "~/Content/StyleLogin.css"));
+                        "~/Content/login/StyleLogin.css"));
+
+            //Bundles de css para templante Bucket
+            bundles.Add(new StyleBundle("~/Content/bucket").Include(
+                        "~/Content/themebucket/bootstrap-reset.css",
+                        "~/Content/themebucket/clndr.css",
+                        "~/Content/themebucket/jquery-jvectormap-1.2.2.css",
+                        "~/Content/themebucket/jquery-ui-1.10.1.custom.min.css",
+                        "~/Content/themebucket/style.css",
+                        "~/Content/themebucket/style-responsive.css"));
+
         }
     }
 }
