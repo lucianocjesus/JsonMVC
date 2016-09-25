8c0e6968983c9b5a90b64d65132cc1e1c54bab48
lucianocjesus - Luciano Carlos <lucianocjesus@gmail.com>
2016-09-25 13:46:52 -0300

    script para tela de login

---
+++ JsonMVC/JsonMVC.Web/Scripts/login/functionlogin.js
@@ -0,0 +1,15 @@
+﻿$(function($) {
+	"use strict";
+
+	$("#BtnForgotPassword").click(function () {
+		$("#lblInformation").find("p").html("Esqueceu sua senha!? <br/>Não se preocupe, preencha o campo abaixo que enviaremos seu acesso por email.");
+
+		$("#modalInfo").modal();
+	});
+
+
+	//Acessar Sistema
+	$("#BtnSignIn").click(function() {
+		
+	});
+});
